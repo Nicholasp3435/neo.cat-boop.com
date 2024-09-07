9845528fe0f38691c1a9ab3c95d6062f6064c831
Nicholas <nap44794@uga.edu>
2024-09-07 00:02:54 -0400

    feat: add tools to drawing and add color picker

--- guestbook/draw.js
+++ guestbook/draw.js
@@ -4,28 +4,50 @@ window.addEventListener('load', () => {
     canvas.addEventListener('mousedown', startPainting); 
     document.addEventListener('mouseup', stopPainting); 
     canvas.addEventListener('mousemove', sketch); 
+    previousColorsContainer.addEventListener('mousedown', selectColor)
 
     // Touch events for touch screens
     canvas.addEventListener('touchstart', startTouchPainting);
     document.addEventListener('touchend', stopPainting);
     canvas.addEventListener('touchmove', touchSketch);
+
+    askIfLoad();
 }); 
     
 const canvas = document.querySelector('#canvas'); 
 canvas.width = 600;
 canvas.height = 300;
 const ctx = canvas.getContext('2d'); 
+
+ctx.fillStyle = '#fff';
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+
 let coord = {x: 0, y: 0};  
 let paint = false; 
+let erasing = false;
+let eyedropping = false;
 
 // max undo stack
-const MAX_UNDO_STATES = 30;
+const MAX_UNDO_STATES = 100;
+
+let undoPointer = 0;
 
 // saves state of each stroke for undoing
 let undoStack = [];
+saveState();
 
-const stroke = document.getElementById("stroke-color");
 const thicc = document.getElementById("thicc");
+const erase = document.getElementById("eraser");
+const eyedrop = document.getElementById("eyedropper");
+
+const previousColorsContainer = document.getElementById("previous-colors");
+var previousColorsElms = [];
+var selectedColorIndex = 0;
+for (let i = 0; i < previousColorsContainer.childElementCount; i++) {
+    previousColorsElms.push(previousColorsContainer.children[i]);
+    previousColorsElms[i].id = i;
+}
 
 thicc.addEventListener("input", (event) => {
     if (event.target.value > 50) {
@@ -55,28 +77,53 @@ function getTouchPosition(touchEvent) {
 
 function startPainting(event){
     if (event.target.nodeName !== "CANVAS") return;
+    if (eyedropping) {
+        getPosition(event); 
+        eyedropPixel();
+        return;
+    }
+    if (document.getElementById("loadbtn")) {
+        document.getElementById("loadbtn").remove();
+    } 
     paint = true; 
-    saveState(); 
     getPosition(event); 
 } 
 
 function startTouchPainting(event) {
     event.preventDefault();
+    if (eyedropping) {
+        getPosition(event); 
+        eyedropPixel();
+        return;
+    }
+    document.getElementById("loadbtn").remove();
     paint = true;
-    saveState();
     getTouchPosition(event);
 }
 
 function stopPainting() {
+    if (paint) {
+        saveState();
+        saveToLocal();
+    }
     paint = false; 
 } 
 
 function sketch(event){ 
     if (!paint) return; 
+    if (undoPointer < undoStack.length) {
+        for (let i = undoPointer; i < undoStack.length; i++) {
+            undoStack.pop();
+        }
+    }
     ctx.beginPath(); 
     ctx.lineWidth = thicc.value; 
     ctx.lineCap = 'round'; 
-    ctx.strokeStyle = stroke.value; 
+    if (!erasing) {
+        ctx.strokeStyle = previousColorsElms[selectedColorIndex].value; 
+    } else {
+        ctx.strokeStyle = '#fff'; 
+    }
     ctx.moveTo(coord.x, coord.y); 
     getPosition(event); 
     ctx.lineTo(coord.x , coord.y); 
@@ -89,7 +136,11 @@ function touchSketch(event) {
     ctx.beginPath();
     ctx.lineWidth = thicc.value;
     ctx.lineCap = 'round';
-    ctx.strokeStyle = stroke.value;
+    if (!erasing) {
+        ctx.strokeStyle = stroke.value; 
+    } else {
+        ctx.strokeStyle = '#fff'; 
+    }
     ctx.moveTo(coord.x, coord.y);
     getTouchPosition(event);
     ctx.lineTo(coord.x, coord.y);
@@ -99,19 +150,101 @@ function touchSketch(event) {
 function saveState() {
     if (undoStack.length >= MAX_UNDO_STATES) {
         undoStack.shift();
+        undoPointer--;
     }
     let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
     undoStack.push(imageData);
+    undoPointer++;
+    console.log(undoStack, undoPointer)
 }
 
 function undo() {
-    if (undoStack.length > 0) {
-        let previousState = undoStack.pop();
+     if (undoPointer > 1) {
+        undoPointer--;
+        let previousState = undoStack[undoPointer - 1];
         ctx.putImageData(previousState, 0, 0);
     }
+    
+    console.log(undoStack, undoPointer)
+}
+
+function redo() {
+    if (undoPointer < undoStack.length) {
+        let nextState = undoStack[undoPointer];
+        ctx.putImageData(nextState, 0, 0);
+        undoPointer++;
+    }
+
+    console.log(undoStack, undoPointer)
 }
 
 function clearDraw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
+function toggleErase() {
+    if (erase.classList.contains("toggled")) {
+        erase.classList.remove("toggled");
+        erasing = false;
+    } else {
+        erasing = true;
+        eyedropping = false;
+        eyedrop.classList.remove("toggled");
+        erase.classList.add("toggled");
+    }
+}
+
+function toggleEyedropper() {
+    if (eyedrop.classList.contains("toggled")) {
+        eyedrop.classList.remove("toggled");
+        eyedropping = false;
+    } else {
+        erasing = false;
+        eyedropping = true;
+        erase.classList.remove("toggled");
+        eyedrop.classList.add("toggled");
+    }
+}
+
+function eyedropPixel() {
+    let [ r, g, b, a ] = ctx.getImageData( coord.x, coord.y, 1, 1 ).data;
+    const color = "#" + r.toString(16) + g.toString(16) + b.toString(16);
+    previousColorsElms[selectedColorIndex].value = color;
+    console.log(r, g, b);
+}
+
+function selectColor(event) {
+    const target = event.target;
+    for (let i = 0; i < previousColorsElms.length; i++) {
+        previousColorsElms[i].classList.remove("toggled");
+    }
+    target.classList.add("toggled");
+    selectedColorIndex = parseInt(target.id);
+    console.log(selectedColorIndex);
+}
+
+function saveToLocal() {
+    if (localStorage.getItem("lateststroke") === null) {
+        localStorage.lateststroke = canvas.toDataURL();
+    }
+    localStorage.lateststroke = canvas.toDataURL();
+}
+
+function loadFromLocal() {
+    var dataURL = localStorage.lateststroke;
+    var img = new Image;
+    img.src = dataURL;
+    img.onload = function () {
+        ctx.drawImage(img, 0, 0);
+    };
+}
+
+function askIfLoad() {
+    if (localStorage.getItem("lateststroke") !== null) {
+        const loadbtn = document.createElement('button');
+        loadbtn.id = "loadbtn";
+        loadbtn.innerHTML = "Detected unsent image.<br>Click to load it?<br>Drawing will remove this message"
+        loadbtn.onclick = function() {loadFromLocal();loadbtn.remove();};
+        document.getElementById("drawing-container").appendChild(loadbtn);
+    }
+}
