13f850dcf4badce21bfa864a3f14e592270aff29
Nicholas <nap44794@uga.edu>
2024-09-27 23:22:34 -0400

    feat: add solar view

--- moonPhase.js
+++ moonPhase.js
@@ -3,6 +3,7 @@ console.log("moonPhase.js");
 const moonDiv = document.getElementById("moonPhase");
 
 const moonImg = document.createElement("img");
+moonImg.classList.add("full-width");
 moonDiv.appendChild(moonImg);
 
 var date = new Date();
@@ -12,11 +13,11 @@ const url = "https://svs.gsfc.nasa.gov/api/dialamoon/" + date;
 
 var r = new XMLHttpRequest();
 
-
 r.onload = function () {
     const json = JSON.parse(r.responseText);
     var moonURL = json['image']['url'];
     moonImg.src = moonURL;
+    moonImg.alt = json['image']['alt_text'];
 };
 
 r.open("GET", url);
